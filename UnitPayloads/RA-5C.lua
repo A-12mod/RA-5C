bcdbe616c073d3db8aaade8b2fcc0db1151e773d
A-12mod <121409989+A-12mod@users.noreply.github.com>
2023-01-08 15:04:24 -0500

    Add files via upload
    /Unitpayloads; /VOIP_Radios

---
+++ UnitPayloads/RA-5C.lua
@@ -0,0 +1,34 @@
+local unitPayloads = {
+	["name"] = "RA-5C",
+	["payloads"] = {
+		[1] = {
+			["name"] = "Fuel 300G X2, Mk82 X1",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "{Fuel 300G}",
+					["num"] = 2,
+				},
+				[2] = {
+					["CLSID"] = "{Mk82}",
+					["num"] = 3,
+				},
+				[3] = {
+					["CLSID"] = "{Fuel 300G}",
+					["num"] = 4,
+			},
+		},
+           [2] = {
+			["name"] = "Fuel 300G X3",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "{Fuel 300G}",
+					["num"] = 2,
+				},
+				[2] = {
+					["CLSID"] = "{Fuel 300G}",
+					["num"] = 3,
+				},
+				[3] = {
+					["CLSID"] = "{Fuel 300G}",
+					["num"] = 4,
+
